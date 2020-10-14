e755581328ff2e0e75b0a237dce5e63e979997be
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-10-14 15:17:46 -0300

    first

---
+++ src/views/orphanages_view.ts
@@ -0,0 +1,23 @@
+import Orphanages from '../models/Orphanage';
+import images_view from './images_view';
+import Images from './images_view';
+
+export default {
+  render(orphanage:Orphanages){
+    return {
+      id: orphanage.id,
+      name: orphanage.name,
+      latitude: orphanage.latitude,
+      longitude: orphanage.longitude,
+      about: orphanage.about,
+      instructions: orphanage.instructions,
+      opening_hours: orphanage.opening_hours,
+      open_on_weekends: orphanage.open_on_weekends,
+      image: images_view.renderMany(orphanage.images)
+    } 
+  },
+
+  renderMany(orphanages: Orphanages[]){
+    return orphanages.map((orphanage) => this.render(orphanage));
+  }
+};
